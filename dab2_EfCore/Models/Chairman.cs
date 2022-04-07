a561ff6b64cfe2337e549ef45d880ee1c1bc78c8
Jeppe Hansen <au591132@uni.au.dk>
2022-04-07 13:31:30 +0200

    Relationships changed in models

--- dab2_EfCore/Models/Chairman.cs
+++ dab2_EfCore/Models/Chairman.cs
@@ -1,11 +1,17 @@
-﻿namespace dab2_EfCore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dab2_EfCore.Models
 {
     public class Chairman
     {
+        [Key]
         public int Member_id { get; set; } //Primary key
         public string Name { get; set; }
         public int Cpr_number { get; set; }
         public string Address { get; set; }
-        public int Cvr_number { get; set; } //Foreign key
+
+
+        public int Cvr_number { get; set; }  //Foreign key
+        public Society Society { get; set; }
     }
 }
--- dab2_EfCore/Models/Member.cs
+++ dab2_EfCore/Models/Member.cs
@@ -1,9 +1,14 @@
-﻿namespace dab2_EfCore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dab2_EfCore.Models
 {
     public class Member
     {
+        [Key]
         public int Member_id { get; set; } //Primary key
         public string Name { get; set; }
+
         public int Cvr_number { get; set; } //Foreign key
+        public Society Society { get; set; }
     }
 }
